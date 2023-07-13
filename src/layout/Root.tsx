b3f5cce49f9ee8ff0cb8372d1bdf95f137184807
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-07-13 21:16:33 +0630

    loading

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -1,20 +1,27 @@
 import Main from 'pages/main'
 import NavBar from './navbar'
 import { Outlet, useNavigation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 function Root() {
   const navigation = useNavigation()
+  const [ready, setready] = useState<Boolean>()
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      setready(true)
+    })
+  }, [])
   return (
     <div>
       <div className="fixed top-0 mx-auto left-0 right-0 z-50">
         <NavBar />
       </div>
-      {navigation.state === 'loading' ? (
-        <div>loading</div>
-      ) : (
+      {ready ? (
         <div className="">
           <Outlet />
         </div>
+      ) : (
+        <div>loading</div>
       )}
     </div>
   )
