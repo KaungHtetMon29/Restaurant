199d0d44b884af03b596db05e49cdf30cc60d79a
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-07-18 20:42:23 +0630

    ani

--- src/pages/Aboutus.tsx
+++ src/pages/Aboutus.tsx
@@ -6,6 +6,7 @@ import cookie from 'Assets/svgs/cookie.svg'
 import waiter from 'Assets/svgs/avatar.svg'
 import heart from 'Assets/svgs/heart.svg'
 import { useEffect } from 'react'
+import { motion } from 'framer-motion'
 function Aboutus() {
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -14,41 +15,51 @@ function Aboutus() {
   return (
     <div className="pt-32 flex flex-col items-center">
       <div className="flex mb-48 mx-82">
-        <div className="flex flex-col grow  font-segoe  mt-20 w-1/2 pr-10">
+        <motion.div
+          className="flex flex-col grow  font-segoe  mt-20 w-1/2 pr-10 "
+          initial={{ opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            transition: { duration: 1, delay: 0.1 }
+          }}
+        >
           <div className="flex text-5xl font-bold  text-custorange">
             <div>About us</div>
             <div className="ml-4">
               <img src={sburger} width={50} />
             </div>
           </div>
-          <div className="mt-12 text-2xl tracking-widest leading-10 pr-8   ">
+          <div className="mt-12 text-2xl tracking-widest leading-10 pr-8 ">
             <span className="font-bold text-3xl">
               Welcome to{' '}
               <span className="text-custorange">Delicious Bites</span>
             </span>{' '}
             - where culinary passion meets warm hospitality. Our diverse menu
-            features <span className="font-bold"> delightful dishes</span>{' '}
-            prepared with the finest local ingredients. From classic favorites
-            to innovative creations, we offer something for every taste. Join us
-            for a memorable dining experience that will leave you craving for
-            more.{' '}
+            features delightful dishes prepared with the finest local
+            ingredients. From classic favorites to innovative creations, we
+            offer something for every taste. Join us for a memorable dining
+            experience that will leave you craving for more.{' '}
             <span className=" text-custorange">
               We can't wait to serve you!
             </span>
           </div>
-        </div>
+        </motion.div>
         <div className="flex w-1/2 pt-20">
-          <div className="w-full ">
+          <motion.div
+            className="w-full "
+            initial={{ scale: 0 }}
+            whileInView={{ scale: 1 }}
+          >
             {' '}
             <img src={burger} width={3000} />
-          </div>
+          </motion.div>
         </div>
       </div>
       <div className="bg-custgray w-full mx-82 flex flex-col items-center pb-48">
-        <div className="text-5xl font-bold font-segoe  mt-32">
-          <h1>Advantages of dining at our restaurant</h1>
+        <div className="text-5xl font-bold font-segoe  mt-48">
+          <h1>What You Will Get</h1>
         </div>
-        <div className="flex mx-82 mt-48">
+        <div className="flex mx-82 mt-24">
           {data.map((e, index) => {
             return <Adv img={icon[index]} head={e.title} para={e.content} />
           })}
--- src/pages/Adv.tsx
+++ src/pages/Adv.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 type adv = {
   img: any
   head: string
@@ -5,7 +6,14 @@ type adv = {
 }
 function Adv({ img, head, para }: adv) {
   return (
-    <div className="flex flex-col w-1/3 mx-4 rounded-2xl shadow-xl border-custorange bg-white px-12 py-8 font-segoe items-center">
+    <motion.div
+      whileHover={{ scale: 1.1, transition: { delay: 0 } }}
+      whileInView={{ y: 0, opacity: 1 }}
+      initial={{ y: 100, opacity: 0 }}
+      transition={{ type: 'spring', delay: 0 }}
+      exit={{ y: 100, opacity: 0 }}
+      className="flex flex-col w-1/3 mx-4 rounded-2xl shadow-xl border-custorange bg-white px-12 py-8 font-segoe items-center"
+    >
       <div>
         <img src={img} width={100} />
       </div>
@@ -15,7 +23,7 @@ function Adv({ img, head, para }: adv) {
       <div className="text-center mt-12 leading-7 text-md">
         <p>{para}</p>
       </div>
-    </div>
+    </motion.div>
   )
 }
 export default Adv
