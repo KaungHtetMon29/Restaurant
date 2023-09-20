faedb4218a258cd147c795489b248ae1fed2c75d
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-09-21 01:16:07 +0630

    test

--- src/components/customercards/customercard.tsx
+++ src/components/customercards/customercard.tsx
@@ -19,24 +19,28 @@ export function Customercard() {
   const customers = [1, 2, 3]
   return (
     <div className="flex  relative ">
-      <div className="flex relative 2xl:mx-82 xl:mx-64 lg:mx-32 overflow-hidden">
+      <div className="flex relative 2xl:mx-82 xl:mx-64 lg:mx-32 md:mx-16 overflow-hidden">
         <div className=" flex relative pb-72 ..." id="test">
           {customers.map((e) => (
             <div className="relative flex mr-5">
-              <div className="absolute 2xl:w-fit xl:w-fit lg:w-fit m-auto left-0 right-0 top-10">
+              <div className="absolute 2xl:w-fit xl:w-fit lg:w-fit md:w-16 m-auto left-0 right-0 lg:top-10 md:top-16">
                 <img src={customer} />
               </div>
-              <div className="bg-white shadow-md mt-24 2xl:pt-32 xl:pt-28 lg:pt-20 2xl:pb-8 xl:pb-6 lg:pb-12 justify-center font-segoe rounded-tl-4xl rounded-tr-3xl rounded-bl-3xl rounded-br-4xl">
+              <div className="bg-white shadow-md mt-24 2xl:pt-32 xl:pt-28 lg:pt-20 md:pt-12 2xl:pb-8 xl:pb-6 lg:pb-12 md:pb-6 justify-center font-segoe rounded-tl-4xl rounded-tr-3xl rounded-bl-3xl rounded-br-4xl">
                 <div className="px-4">
-                  <p className="text-center font-normal leading-6 px-4 2xl:text-sm xl:text-sm lg:text-sm ...">
+                  <p className="text-center font-normal leading-6 px-4 2xl:text-sm xl:text-sm lg:text-sm md:text-xs ...">
                     Lorem ipsum dolor sit amet consectetur. Tortor massa nisl
                     quam sit. Vitae congue ultrices neque penatibus mi in
                     quisque. Leo in cursus enim magnis ante. Proin iaculis
                     platea ipsum sagittis ac eu aliquam quis. Ornare tincidunt
                     tempus semper
                   </p>
-                  <p className="text-center font-bold pt-6">Ama Ampomah</p>
-                  <p className="text-center text-sm">CEO & Founder Inc </p>
+                  <p className="text-center font-bold pt-6 lg:text-base md:text-sm">
+                    Ama Ampomah
+                  </p>
+                  <p className="text-center lg:text-sm md:text-xs">
+                    CEO & Founder Inc{' '}
+                  </p>
                 </div>
               </div>
             </div>
--- src/pages/happycustomer.tsx
+++ src/pages/happycustomer.tsx
@@ -4,10 +4,10 @@ export function Hcustomer() {
   return (
     <div className="pb-20">
       <div>
-        <div className="2xl:text-7xl xl:text-5xl lg:text-4xl pt-32 text-center">
+        <div className="2xl:text-7xl xl:text-5xl lg:text-4xl md:text-3xl pt-32 text-center">
           <p>Our Happy Customers</p>
         </div>
-        <div className=" pt-20 font-segoe 2xl:font-light xl:font-normal text-xl">
+        <div className=" lg:pt-20 md:pt-10 font-segoe 2xl:font-light xl:font-normal md:font-normal text-xl">
           <p className="text-center">
             Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed
             do eiusmod tempor incididunt
